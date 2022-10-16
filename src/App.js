c71f8d7a1e2904400122119ee3cf9743be129f3c
Shahrukh Showkath <shahrukhshowkath@gmail.com>
2022-10-16 14:43:42 -0500

    Further cleanup of default app to create barebone web page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <h1>Fantasy Football App</h1>
+      <h2>Home Page</h2>
     </div>
   );
 }
