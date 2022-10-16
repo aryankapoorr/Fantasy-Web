7713bbdbb1f5d7da03c8fe040ba7b52b02a71d5b
Shahrukh Showkath <shahrukhshowkath@gmail.com>
2022-10-16 15:46:24 -0500

    added navigation bar to top of page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Fantasy Football App</h1>
-      <h2>Home Page</h2>
-    </div>
+    <Navbar>
+      <NavItem icon="A"></NavItem>
+      <NavItem icon="B"></NavItem>
+      <NavItem icon="C"></NavItem>
+      <NavItem icon="Dropdown">
+        <DropdownMenu />
+      </NavItem>
+    </Navbar>
   );
 }
+
+function Navbar(props) {
+  return (
+    <nav className="navbar">
+      <ul className ="navbar-nav">{ props.children }</ul>
+    </nav>
+  )
+}
+
+function NavItem(props) {
+  const[open, setOpen] = useState(false);
+
+
+  return(
+    <li className="nav-item">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+      {open && props.children}
+    </li>
+  )
+}
+
+function DropdownMenu() {
+
+  function DropdownItem(props) {
+    return <a href="#" className='menu-item'>
+      {props.children}
+    </a>
+  }
+  return (
+    
+    <div className='dropdown'>
+        <DropdownItem>
+          adfdfdbc
+        </DropdownItem>
+    </div>
+  )
+}
+
 
 export default App;
